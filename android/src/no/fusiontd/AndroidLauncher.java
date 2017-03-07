62af154de2a97d0bb9b770ee07e159c114d85194
simen <simennj@outlook.com>
2017-03-07 14:26:38 +0100

    init

---
+++ android/src/no/fusiontd/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package no.fusiontd;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.fusiontd.FusionTD;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FusionTD(), config);
+	}
+}
---
+++ desktop/src/no/fusiontd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.fusiontd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.fusiontd.FusionTD;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FusionTD(), config);
+	}
+}
