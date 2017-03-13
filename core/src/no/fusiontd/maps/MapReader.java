b68ad3004dd6741db973bdfa6d7f508a4905a50e
simen <simennj@outlook.com>
2017-03-13 23:41:19 +0100

    add comment to mapreader

--- core/src/no/fusiontd/maps/MapReader.java
+++ core/src/no/fusiontd/maps/MapReader.java
@@ -8,25 +8,33 @@ public class MapReader {
 
         int[][] map = new int[rows][cols];
 
+        // \\R matches any newline char
         String[] level = Gdx.files.internal("maps/" + mapName).readString().split("\\R");
 
         for (int y = 0; y < rows; y++) {
             char[] row = level[y].toCharArray();
             for (int x = 0; x < cols; x++) {
                 switch (row[x]) {
-                    case '0': map[y][x] = 0;
+                    case '0':
+                        map[y][x] = 0;
                         break;
-                    case '1': map[y][x] = 1;
+                    case '1':
+                        map[y][x] = 1;
                         break;
-                    case '2': map[y][x] = 2;
+                    case '2':
+                        map[y][x] = 2;
                         break;
-                    case '3': map[y][x] = 3;
+                    case '3':
+                        map[y][x] = 3;
                         break;
-                    case '4': map[y][x] = 4;
+                    case '4':
+                        map[y][x] = 4;
                         break;
-                    case '5': map[y][x] = 5;
+                    case '5':
+                        map[y][x] = 5;
                         break;
-                    default: map[y][x] = 0;
+                    default:
+                        map[y][x] = 0;
                 }
             }
         }
