ef89cdea8a1d4115bb0248905c8edb57704d0fd5
simen <simennj@outlook.com>
2017-03-13 23:39:24 +0100

    further cleanup of mapreader

--- core/src/no/fusiontd/maps/MapReader.java
+++ core/src/no/fusiontd/maps/MapReader.java
@@ -8,12 +8,12 @@ public class MapReader {
 
         int[][] map = new int[rows][cols];
 
-        String level = Gdx.files.internal("maps/" + mapName).readString().replace("\r\n", "\n").replace("\r", "\n").replace("\n", "");
-        char[] oneline = level.toCharArray();
+        String[] level = Gdx.files.internal("maps/" + mapName).readString().split("\\R");
 
         for (int y = 0; y < rows; y++) {
+            char[] row = level[y].toCharArray();
             for (int x = 0; x < cols; x++) {
-                switch (oneline[x + y*cols]) {
+                switch (row[x]) {
                     case '0': map[y][x] = 0;
                         break;
                     case '1': map[y][x] = 1;
