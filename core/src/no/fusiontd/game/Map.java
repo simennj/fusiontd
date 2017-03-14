662f3a7418cf3c2904a9c08653723963449cd89b
Even Grøtte <eogrotte@stud.ntnu.no>
2017-03-14 09:36:25 +0100

    Implemented the pathFinder function

--- core/src/no/fusiontd/game/Map.java
+++ core/src/no/fusiontd/game/Map.java
@@ -4,6 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 import no.fusiontd.maps.MapReader;
 
@@ -57,6 +65,77 @@ public class Map {
     public void creepSpawn(int spawnNumber){
     }
 
-    public void pathFinder(){
+    public static List<Point2D> findPath(int[][] adj){
+
+
+        // Remembers which cells have been checked
+        boolean[][] checked = new boolean[TILECOLS][TILEROWS]; // default value is false
+
+        List<Point2D> points = new ArrayList<Point2D>();
+        Point2D startPoint = null;
+        Point2D endPoint = null;
+
+        //Iterate over matrix to find start and end point
+        for (int i = 0; i < TILECOLS; i++){
+            for (int j = 0; j < TILEROWS; j++){
+
+                if (adj[i][j] == 4){
+
+                    startPoint = new Point2D.Double(i, j);
+                    points.add(startPoint);
+                    checked[i][j] = true;
+
+                }
+
+                if (adj[i][j] == 5){
+                    endPoint = new Point2D.Double(i, j);
+                    checked[i][j] = true;
+                }
+
+            }
+        }
+
+        //Start search
+        Queue<Point2D> queue = new ArrayDeque<Point2D>();
+        queue.add(startPoint);
+
+        while (!queue.isEmpty()){
+
+            Point2D current = queue.remove();
+            int x = (int) current.getX();
+            int y = (int) current.getY();
+
+            if (x - 1 >= 0 && adj[x - 1][y] == 1 && !checked[x - 1][y]){
+
+                Point2D point = new Point2D.Double(x - 1, y);
+                queue.add(point);
+                points.add(point);
+                checked[x - 1][y] = true;
+            }
+            if (x + 1 < TILECOLS && adj[x + 1][y] == 1  && !checked[x + 1][y]){
+
+                Point2D point = new Point2D.Double(x + 1, y);
+                queue.add(point);
+                points.add(point);
+                checked[x + 1][y] = true;
+            }
+            if (y - 1 >= 0 && adj[x][y - 1] == 1 && !checked[x][y - 1]){
+
+                Point2D point = new Point2D.Double(x, y - 1);
+                queue.add(point);
+                points.add(point);
+                checked[x][y - 1] = true;
+            }
+            if (y + 1 < TILEROWS && adj[x][y + 1] == 1 && !checked[x][y + 1]){
+
+                Point2D point = new Point2D.Double(x, y + 1);
+                queue.add(point);
+                points.add(point);
+                checked[x][y + 1] = true;
+            }
+        }
+        points.add(endPoint);
+
+        return points;
     }
 }
