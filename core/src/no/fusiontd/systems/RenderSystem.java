c9748b448d63ef1e5bb46acea2f0c649871beb05
simen <simennj@outlook.com>
2017-03-20 12:52:49 +0100

    add rotation to required components for RenderSystem

--- core/src/no/fusiontd/systems/RenderSystem.java
+++ core/src/no/fusiontd/systems/RenderSystem.java
@@ -1,18 +1,14 @@
 package no.fusiontd.systems;
 
 import com.artemis.Aspect;
-import com.artemis.EntitySystem;
-import com.artemis.utils.Bag;
-import com.artemis.utils.Sort;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.artemis.ComponentMapper;
 import com.artemis.Entity;
-import javax.swing.Renderer;
-import java.util.Comparator;
+import com.artemis.EntitySystem;
+import com.artemis.utils.Bag;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import no.fusiontd.components.Position;
 import no.fusiontd.components.Render;
 import no.fusiontd.components.Rotation;
-import no.fusiontd.components.Transform;
 
 public class RenderSystem extends EntitySystem {
     private final SpriteBatch batch;
@@ -20,8 +16,9 @@ public class RenderSystem extends EntitySystem {
     ComponentMapper<Position> mPos;
     ComponentMapper<Render> mRend;
     ComponentMapper<Rotation> mRot;
+
     public RenderSystem(SpriteBatch batch) {
-        super(Aspect.all(Position.class, Render.class));
+        super(Aspect.all(Position.class, Rotation.class, Render.class));
         this.batch = batch;
     }
 
