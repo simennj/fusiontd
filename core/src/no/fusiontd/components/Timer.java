78fb658d88cf98a4c4b83d19a9a6cac854ee5118
Andreas Borhaug <andreas.borhaug@outlook.com>
2017-03-21 15:03:37 +0100

    Timer component added

---
+++ core/src/no/fusiontd/components/Timer.java
@@ -0,0 +1,7 @@
+package no.fusiontd.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class Timer implements Component {
+    public float time;
+}
--- core/src/no/fusiontd/systems/CollisionSystem.java
+++ core/src/no/fusiontd/systems/CollisionSystem.java
@@ -6,7 +6,8 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.ashley.utils.ImmutableArray;
-import no.fusiontd.components.Collision;
+
+import no.fusiontd.components.Durability;
 import no.fusiontd.components.Position;
 import no.fusiontd.components.Type;
 import no.fusiontd.components.Attack;
@@ -18,7 +19,9 @@ public class CollisionSystem extends IteratingSystem {
     ComponentMapper<Attack> mAttack;
     ComponentMapper<Attackable> mAttab;
     ComponentMapper<Position>  mPos;
+    ComponentMapper<Durability> mDur;
     float dx, dy, distance;
+    int creepLife, projLife;
 
     public CollisionSystem() {
         super(Family.all(Attack.class, Position.class, Type.class).get());
@@ -31,10 +34,10 @@ public class CollisionSystem extends IteratingSystem {
         Attack projAtt = mAttack.get(entity);
         for (Entity creep : creeps) {
 
-            dx = projPos.x - creep.getComponent(Position.class).x;
-            dy = projPos.y - creep.getComponent(Position.class).y;
+            dx = projPos.x - mPos.get(creep).x;
+            dy = projPos.y - mPos.get(creep).y;
             distance = (float)Math.sqrt(dx*dx + dy*dy);
-            if(distance < projAtt.projradius + creep.getComponent(Attackable.class).creepradius) {
+            if(distance < projAtt.projradius + mAttab.get(creep).creepradius) {
                 CollisionHandler(entity, creep);
             }
             
@@ -42,6 +45,23 @@ public class CollisionSystem extends IteratingSystem {
     }
 
     void CollisionHandler(Entity proj, Entity creep) {
+        if(mDur.has(proj)) {
+            System.out.println("Kollisjon");
+            creepLife = mDur.get(creep).life;
+            projLife = mDur.get(proj).life;
+
+            mDur.get(creep).life -= projLife;
+            mDur.get(proj).life -= creepLife;
+
+            if (mDur.get(creep).life <= 0) {
+                creep.removeAll();
+            }
+            if (mDur.get(proj).life <= 0) {
+                proj.removeAll();
+            }
+        }
+
+
 
     }
 }
