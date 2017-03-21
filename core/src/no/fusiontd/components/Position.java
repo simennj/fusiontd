69d7156f90f5cc235ee2fc75683f7ca1b3097830
simen <simennj@outlook.com>
2017-03-21 17:59:42 +0100

    add constructors to position and velocity components

--- core/src/no/fusiontd/components/Position.java
+++ core/src/no/fusiontd/components/Position.java
@@ -5,4 +5,12 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Position extends Vector2 implements Component {
 
+    public Position() {
+        this(0, 0);
+    }
+
+    public Position(float x, float y) {
+        super(x, y);
+    }
+
 }
--- core/src/no/fusiontd/components/Velocity.java
+++ core/src/no/fusiontd/components/Velocity.java
@@ -6,4 +6,12 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Velocity extends Vector2 implements Component {
 
+    public Velocity() {
+        this(0, 0);
+    }
+
+    public Velocity(float x, float y) {
+        super(x, y);
+    }
+
 }
