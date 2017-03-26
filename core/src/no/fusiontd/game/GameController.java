5c6cff47221d41dc75467bb06c6bb193f2b8ea1d
simen <simennj@outlook.com>
2017-03-26 22:32:13 +0200

    move input handling back into view

--- core/src/no/fusiontd/game/GameController.java
+++ core/src/no/fusiontd/game/GameController.java
@@ -1,9 +1,8 @@
 package no.fusiontd.game;
 
-import com.badlogic.gdx.InputProcessor;
 import no.fusiontd.screens.PlayScreen;
 
-public class GameController implements InputProcessor {
+public class GameController {
 
     private Map model;
     private PlayScreen view;
@@ -13,50 +12,4 @@ public class GameController implements InputProcessor {
         this.view = view;
     }
 
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        int tile = model.getTile(view.getCameraY(screenY), view.getCameraX(screenX));
-        if (tile == 4) {
-            return false;
-        } else if (tile == 0 || tile == 2 || tile == 3) {
-            model.placeTower(view.getCameraX(screenX), view.getCameraY(screenY));
-            return false;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
-    }
 }
--- core/src/no/fusiontd/screens/PlayScreen.java
+++ core/src/no/fusiontd/screens/PlayScreen.java
@@ -2,6 +2,7 @@ package no.fusiontd.screens;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -17,7 +18,7 @@ import no.fusiontd.game.EntityComponentManager;
 import no.fusiontd.game.GameController;
 import no.fusiontd.game.Map;
 
-public class PlayScreen implements Screen {
+public class PlayScreen implements Screen, InputProcessor {
 
     public static final float WIDTH = 16, HEIGHT = 9;
     public float w, h;
@@ -46,7 +47,7 @@ public class PlayScreen implements Screen {
         map = new Map();
         tilesize = Math.min(WIDTH / map.TILECOLS, HEIGHT / map.TILEROWS);
         controller = new GameController(map, this);
-        Gdx.input.setInputProcessor(controller);
+        Gdx.input.setInputProcessor(this);
         batch = new SpriteBatch();
         initializeTextures();
         engine = new EntityComponentManager(this);
@@ -156,6 +157,53 @@ public class PlayScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        int tile = map.getTile(getCameraY(screenY), getCameraX(screenX));
+        if (tile == 4) {
+            return false;
+        } else if (tile == 0 || tile == 2 || tile == 3) {
+            map.placeTower(getCameraX(screenX), getCameraY(screenY));
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 
     public enum State {
