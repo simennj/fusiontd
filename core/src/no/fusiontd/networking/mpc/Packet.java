e23c48b067111740ed98a53f01e55f6f4c762dd2
Odd <oddh123@gmail.com>
2017-03-28 16:18:39 +0200

    Deleted mps, added more packets in Packet, client logic more or less finished

--- core/src/no/fusiontd/networking/mpc/Packet.java
+++ core/src/no/fusiontd/networking/mpc/Packet.java
@@ -1,11 +1,12 @@
 package no.fusiontd.networking.mpc;
 
-/**
- * Created by Odd on 07.03.2017.
- */
 
 public class Packet {
     public static class Packet0LoginRequest {}
     public static class Packet1LoginAnswer {boolean accepted = false;}
     public static class Packet2Message { String message;}
+    public static class Packet3Creep { int creepnumber;}
+    public static class Packet4Lives { int lives;}
+    public static class Packet5score { int score;}
+    public static class Packet6HighScore{ String player; int finalScore;}
 }
