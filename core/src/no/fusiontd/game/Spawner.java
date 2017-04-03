b7216cab2c89099899c492d3508a74be97556043
simen <simennj@outlook.com>
2017-04-03 13:22:57 +0200

    add PoC creep spawner

---
+++ core/src/no/fusiontd/game/Spawner.java
@@ -0,0 +1,34 @@
+package no.fusiontd.game;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Path;
+import com.badlogic.gdx.math.Vector2;
+import no.fusiontd.components.PathFollow;
+import no.fusiontd.components.Position;
+import no.fusiontd.components.Render;
+import no.fusiontd.components.Rotation;
+
+public class Spawner {
+    Path<Vector2> path;
+    Engine engine;
+
+    public Spawner(Path<Vector2> path, Engine engine) {
+        this.path = path;
+        this.engine = engine;
+    }
+
+    public void spawnCreep(TextureAtlas.AtlasRegion texture, Component... components) {
+        Entity creep = new Entity()
+                .add(new Position())
+                .add(new Rotation())
+                .add(new Render(texture))
+                .add(new PathFollow(path));
+        for (Component component : components) {
+            creep.add(component);
+        }
+        engine.addEntity(creep);
+    }
+}
