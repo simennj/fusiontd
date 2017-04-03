4b43dbdabbc5941878fad3deb326b46fb7c57a66
simen <simennj@outlook.com>
2017-04-03 17:57:24 +0200

    add TargetingSystem to engine

--- core/src/no/fusiontd/game/EntityComponentManager.java
+++ core/src/no/fusiontd/game/EntityComponentManager.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Engine;
 import no.fusiontd.screens.PlayScreen;
 import no.fusiontd.systems.PathSystem;
 import no.fusiontd.systems.RenderSystem;
+import no.fusiontd.systems.TargetingSystem;
 import no.fusiontd.systems.VelocitySystem;
 
 public class EntityComponentManager extends Engine {
@@ -13,5 +14,6 @@ public class EntityComponentManager extends Engine {
         addSystem(new VelocitySystem());
         addSystem(new RenderSystem(view.batch));
         addSystem(new PathSystem());
+        addSystem(new TargetingSystem());
     }
 }
