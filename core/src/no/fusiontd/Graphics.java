a81318965ab33695e6eaf3368e7e4be2a2751d70
simen <simennj@outlook.com>
2017-04-03 17:56:06 +0200

    add static containerclass for atlasregions

---
+++ core/src/no/fusiontd/Graphics.java
@@ -0,0 +1,24 @@
+package no.fusiontd;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.ObjectMap;
+
+public class Graphics {
+    private static final ObjectMap<String, TextureAtlas.AtlasRegion> regions;
+    private static TextureAtlas tileAtlas = new TextureAtlas("tiles.atlas");
+
+    static {
+        regions = new ObjectMap<String, TextureAtlas.AtlasRegion>();
+        regions.put("groundTex", tileAtlas.findRegion("024"));
+        regions.put("roadTex", tileAtlas.findRegion("050"));
+        regions.put("bush", tileAtlas.findRegion("131"));
+        regions.put("pathStartTex", tileAtlas.findRegion("091"));
+        regions.put("pathEndTex", tileAtlas.findRegion("090"));
+        regions.put("plane", tileAtlas.findRegion("271"));
+    }
+
+    public static TextureAtlas.AtlasRegion getRegion(String name) {
+        return regions.get(name);
+    }
+
+}
