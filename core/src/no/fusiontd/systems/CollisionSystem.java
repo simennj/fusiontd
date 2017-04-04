d92a11d241a69216fbc485616d1f154a2c2c36b0
simen <simennj@outlook.com>
2017-04-04 10:02:05 +0200

    fix some bugs in collisionSystem

--- core/src/no/fusiontd/systems/CollisionSystem.java
+++ core/src/no/fusiontd/systems/CollisionSystem.java
@@ -2,34 +2,30 @@ package no.fusiontd.systems;
 
 
 import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.ashley.utils.ImmutableArray;
-
-import no.fusiontd.components.Durability;
-import no.fusiontd.components.Position;
-import no.fusiontd.components.Timer;
-import no.fusiontd.components.Type;
-import no.fusiontd.components.Attack;
-import no.fusiontd.components.Attackable;
-import no.fusiontd.components.Position;
-import no.fusiontd.components.Type;
+import no.fusiontd.components.*;
 
 public class CollisionSystem extends IteratingSystem {
 
-    private final ImmutableArray<Entity> creeps;
-    ComponentMapper<Attack> mAttack;
-    ComponentMapper<Attackable> mAttab;
-    ComponentMapper<Position>  mPos;
-    ComponentMapper<Durability> mDur;
-    ComponentMapper<Timer> mTime;
-    float dx, dy, distance;
-    int creepLife, projLife;
+    ComponentMapper<Attack> mAttack = ComponentMapper.getFor(Attack.class);
+    ComponentMapper<Attackable> mAttab = ComponentMapper.getFor(Attackable.class);
+    ComponentMapper<Position> mPos = ComponentMapper.getFor(Position.class);
+    ComponentMapper<Durability> mDur = ComponentMapper.getFor(Durability.class);
+    ComponentMapper<Timer> mTime = ComponentMapper.getFor(Timer.class);
+    private ImmutableArray<Entity> creeps;
 
     public CollisionSystem() {
-        super(Family.all(Attack.class, Position.class, Type.class).get());
-        creeps = getEngine().getEntitiesFor(Family.all(Attackable.class, Position.class, Type.class).get());
+        super(Family.all(Attack.class, Position.class, Durability.class).get());
+    }
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        super.addedToEngine(engine);
+        creeps = getEngine().getEntitiesFor(Family.all(Attackable.class, Position.class, Durability.class).get());
     }
 
     @Override
@@ -37,10 +33,9 @@ public class CollisionSystem extends IteratingSystem {
         Position projPos = mPos.get(entity);
         Attack projAtt = mAttack.get(entity);
         for (Entity creep : creeps) {
-
-            dx = projPos.x - mPos.get(creep).x;
-            dy = projPos.y - mPos.get(creep).y;
-            distance = (float)Math.sqrt(dx*dx + dy*dy);
+            float dx = projPos.x - mPos.get(creep).x;
+            float dy = projPos.y - mPos.get(creep).y;
+            float distance = (float) Math.sqrt(dx * dx + dy * dy);
             if(distance < projAtt.projradius + mAttab.get(creep).creepradius) {
                 CollisionHandler(entity, creep);
             }
@@ -48,19 +43,18 @@ public class CollisionSystem extends IteratingSystem {
         }
     }
 
-    void CollisionHandler(Entity proj, Entity creep) {
-            System.out.println("Kollisjon");
-            creepLife = mDur.get(creep).life;
-            projLife = mDur.get(proj).life;
+    private void CollisionHandler(Entity proj, Entity creep) {
+        float creepLife = mDur.get(creep).life;
+        float projLife = mDur.get(proj).life;
 
             mDur.get(creep).life -= projLife;
             mDur.get(proj).life -= creepLife;
 
             if (mDur.get(creep).life <= 0) {
-                creep.removeAll();
+                getEngine().removeEntity(creep);
             }
             if (mDur.get(proj).life <= 0) {
-                proj.removeAll();
+                getEngine().removeEntity(proj);
             }
 
 
