adaacd52006aca2ef4cd0e8fe9cc1c81c39038cc
simen <simennj@outlook.com>
2017-04-04 17:39:16 +0200

    add vector2 constructor to position

--- core/src/no/fusiontd/components/Position.java
+++ core/src/no/fusiontd/components/Position.java
@@ -9,6 +9,10 @@ public class Position extends Vector2 implements Component {
         this(0, 0);
     }
 
+    public Position(Vector2 vector2) {
+        this(vector2.x, vector2.y);
+    }
+
     public Position(float x, float y) {
         super(x, y);
     }
