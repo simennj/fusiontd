930d8a31f6a80e999e4ad5d98b0bbedc4b72d181
simen <simennj@outlook.com>
2017-04-05 14:29:56 +0200

    add listener for AddOnRemove component

--- core/src/no/fusiontd/game/EntityComponentManager.java
+++ core/src/no/fusiontd/game/EntityComponentManager.java
@@ -1,6 +1,9 @@
 package no.fusiontd.game;
 
-import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.*;
+import no.fusiontd.components.AddOnRemove;
+import no.fusiontd.components.Position;
+import no.fusiontd.components.Rotation;
 import no.fusiontd.screens.PlayScreen;
 import no.fusiontd.systems.*;
 
@@ -14,5 +17,20 @@ public class EntityComponentManager extends Engine {
         addSystem(new TargetingSystem());
         addSystem(new TimerSystem());
         addSystem(new CollisionSystem());
+        addEntityListener(Family.all(AddOnRemove.class, Position.class).get(), new EntityListener() {
+            ComponentMapper<AddOnRemove> removeActionMapper = ComponentMapper.getFor(AddOnRemove.class);
+            ComponentMapper<Position> positionMapper = ComponentMapper.getFor(Position.class);
+            ComponentMapper<Rotation> rotationMapper = ComponentMapper.getFor(Rotation.class);
+
+            @Override
+            public void entityAdded(Entity entity) {
+
+            }
+
+            @Override
+            public void entityRemoved(Entity entity) {
+
+            }
+        });
     }
 }
