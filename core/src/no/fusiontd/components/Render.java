25feb3b8789da8bf953ee8da940a8a9d404681c8
simen <simennj@outlook.com>
2017-04-21 17:49:00 +0200

    fix nullPointerExpection when trying to clone render withouth animation

--- core/src/no/fusiontd/components/Render.java
+++ core/src/no/fusiontd/components/Render.java
@@ -1,5 +1,6 @@
 package no.fusiontd.components;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import no.fusiontd.CloneableComponent;
@@ -7,12 +8,17 @@ import no.fusiontd.Graphics;
 
 public class Render extends Sprite implements CloneableComponent<Render> {
 
+    Animation<TextureRegion> animation;
+
     public Render(String name) {
         this(Graphics.getRegion(name));
     }
 
-    public Render(Sprite sprite) {
-        super(sprite);
+    public Render(Render render) {
+        super(render);
+        if (render.animation != null) {
+            this.animation = cloneAnimation(render.animation);
+        }
     }
 
     public Render(TextureRegion rect) {
@@ -22,6 +28,15 @@ public class Render extends Sprite implements CloneableComponent<Render> {
     public Render(TextureRegion rect, float w, float h) {
         super(rect);
         setScale(1, 1);
+    }
+
+    private static Animation<TextureRegion> cloneAnimation(Animation<TextureRegion> original) {
+        Animation<TextureRegion> clone = new Animation<TextureRegion>(
+                original.getFrameDuration(),
+                original.getKeyFrames()
+        );
+        clone.setPlayMode(original.getPlayMode());
+        return clone;
     }
 
     @Override
