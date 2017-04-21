08d670387b8867608327ddb5bb3af7b7dff57e2b
simen <simennj@outlook.com>
2017-04-21 14:53:29 +0200

    fix cloneComponent in Upgradeable

--- core/src/no/fusiontd/components/Upgradeable.java
+++ core/src/no/fusiontd/components/Upgradeable.java
@@ -1,28 +1,29 @@
 package no.fusiontd.components;
 
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
-
-import java.util.Arrays;
-import java.util.List;
-
 import no.fusiontd.CloneableComponent;
 
-/**
- * Created by Andreas on 19.04.2017.
- */
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Upgradeable implements CloneableComponent<Upgradeable> {
     public float cost;
     public List<CloneableComponent> upgrades;
-    public Upgradeable(float cost, List<CloneableComponent> upgrades){
+
+    public Upgradeable(float cost, List<CloneableComponent> upgrades) {
         this.cost = cost;
         this.upgrades = upgrades;
     }
+
     public Upgradeable(float cost, CloneableComponent... cloneableComponents) {
         this(cost, Arrays.asList(cloneableComponents));
     }
+
     public Upgradeable cloneComponent() {
-        return new Upgradeable(cost, upgrades);
+        List<CloneableComponent> clonedUpgrades = new LinkedList<CloneableComponent>();
+        for (CloneableComponent component : upgrades) {
+            clonedUpgrades.add(component.cloneComponent());
+        }
+        return new Upgradeable(cost, clonedUpgrades);
     }
 }
